f4714f63eda5adae07300c675f95a689558b01c9
G-Houlmann <gildas.houlmann@gmail.com>
2019-05-28 16:48:16 +0200

    fichiers initiaux

---
+++ gestionPort.h
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   gestionPort.h
+ * Author: gilda
+ *
+ * Created on 28. mai 2019, 16:45
+ */
+
+#ifndef GESTIONPORT_H
+#define GESTIONPORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GESTIONPORT_H */
+
